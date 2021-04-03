7c2feadffd0addaa42b1fa5d40e5007ca9cdaaed
Maxim <xbultazar@gmail.com>
2021-04-03 19:34:11 +0300

    css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="http://www.annelishundsim.se/wp-content/uploads/2017/04/webicon.png" alt="logo"/>
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://littletravel.ru/wp-content/uploads/2020/05/azoren_254083921_1920x1280.jpg" alt="image"/>
             </div>
             <div>
                 ava + descr
             </div>
-            <div>
+            <div className={s.posts}>
                 my posts
                 <div>
                     new post
                 </div>
-                <div>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
