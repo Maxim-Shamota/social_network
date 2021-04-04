b4abaea83d51e94b7654356299a4ff6b84fd49ce
Maxim <xbultazar@gmail.com>
2021-04-04 22:15:17 +0300

    structure folders

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://www.peoples.ru/champions/lucky_rich/rich_1.jpg" alt=""/>
+            post 1
+        </div>
+    )
+}
+
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyPosts from './MyPosts/Post/Post';
 import s from './Profile.module.css';
 
 const Profile = () => {
@@ -10,18 +11,7 @@ const Profile = () => {
             <div>
                 ava + descr
             </div>
-            <div className={s.posts}>
-                my posts
-                <div>
-                    new post
-                </div>
-                <div className={s.item}>
-                    post 1
-                </div>
-                <div className={s.item}>
-                    post 2
-                </div>
-            </div>
+            <MyPosts />
         </div>
     )
 }
