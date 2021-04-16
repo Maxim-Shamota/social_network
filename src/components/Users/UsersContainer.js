a7fc7a38e2f85f4d964512897ce9a85362302837
Maxim <xbultazar@gmail.com>
2021-04-16 18:46:31 +0300

    add UsersPage

---
+++ src/components/Users/UsersContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import u from "./Users.module.css";
+
+let Users = (props) => {
+    return (
+        <div className={u.main}>
+            Users will be here
+        </div>
+    )
+}
+
+export default Users
